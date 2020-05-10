46b773ca104f83c2c211ef84b1419a98863a5232
sfylalala <38581204+sfylalala@users.noreply.github.com>
2020-05-11 01:29:12 +0800

    Update main.cpp

--- VerifySignature/main.cpp
+++ VerifySignature/main.cpp
@@ -1,10 +1,13 @@
 #include <QtCore/QCoreApplication>
 #include<qdebug.h>
+#include<qfile.h>
 
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
-	QDebug() << "sfy";
+	qDebug() << "sfy";
+	QFile peFile = QFile("d:test.exe");
+	QByteArray peData = peFile.readAll();
 
 	return a.exec();
 }
